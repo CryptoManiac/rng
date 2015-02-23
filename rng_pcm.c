e26737118bf917a5c34903fa6c9dc72b1c70d045
root <root@ubtu>
2015-02-23 14:04:44 +0000

    Handle PCM samples correctly
    * Each PCM sample is actually 16-bit unsigned integer;
    * Skip unwanted samples which could be affected by latency of GM tube.

--- rng_pcm.c
+++ rng_pcm.c
@@ -4,8 +4,13 @@
 #define __STDC_FORMAT_MACROS
 #include <inttypes.h>
 
-uint64_t curr_pos=0, prev_zero_pos=0,
-         pos1=0, pos2=0, pos3=0, pos4=0;
+
+#define SAMPLING_FREQ 48000
+#define BYTES_PER_SAMPLE 2
+#define PULSE_DURATION 260 / 1000000
+
+#define PULSE_SAMPLES SAMPLING_FREQ * PULSE_DURATION
+
 
 void WriteBit (int bit, FILE *f)
 {
@@ -32,22 +37,41 @@ main (int argc, char *argv[])
 {
     FILE *f = fopen("out.bin", "ab+");
 
-    uint8_t  curr_val = 0;
     uint8_t  count = 0;
+    uint16_t curr_val = 0;
+
+    uint64_t curr_pos=0,
+             pos1=0,
+             pos2=0,
+             pos3=0,
+             pos4=0,
+             next_pos = 0;
+
+    size_t res = 0;
 
     for(;;) {
-        curr_val = fgetc(stdin);
+        res = fread(&curr_val, sizeof(uint16_t), 1, stdin);
 
-        if (feof(stdin))
-            continue;
+        if (res != 1) {
+            printf("\nSample data doesn't provide sufficient amount of bytes!\n");
+            break;
+        }
 
-        if (curr_pos < 44) {
+        if (feof(stdin)) {
+            printf("\nNo sample data left.\n");
+            break;
+        }
+
+        if (curr_pos < next_pos) {
+//            printf("skipping sample %" PRIu64 ", %" PRIu64 " pulse samples left\n", curr_pos, (next_pos - curr_pos));
             curr_pos++;
             continue;
         }
 
+        if (curr_val < 0x7F00) {
 
-        if (curr_val == 0) {
+//            printf("val=%x, distance=%" PRIu64 "\n", curr_val, curr_pos - prev_zero_pos);
+
             switch(count++) {
             case 0:
                 pos1 = curr_pos;
@@ -74,12 +98,16 @@ main (int argc, char *argv[])
                 }
                 fflush(stdout);
 
+                count = 0;
+                pos1 = pos2 = pos3 = pos4 = 0;
+
                 break;
             default:
                 count = 0;
                 pos1 = pos2 = pos3 = pos4 = 0;
             }
-            prev_zero_pos = curr_pos;
+
+            next_pos = curr_pos + PULSE_SAMPLES;
         }
 
         curr_pos++;
